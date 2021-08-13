0f29a6239199d075c09059f8dff197f0b1998742
alnmaurofranco <jefeson23@live.com>
2021-08-13 17:52:14 -0300

    🎉 project finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursoComponent } from './curso/curso.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoComponent,
+    ListagemComponent
   ],
   imports: [
     BrowserModule
